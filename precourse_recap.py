78a8d758ce1a00a002fd6ee07bae835198bf5868
Kieran Rooney <kieranrooney@Kierans-MacBook-Pro.local>
2022-05-02 17:49:55 +0100

    git commit -a

---
+++ precourse_recap.py
@@ -0,0 +1,14 @@
+>>> number_1 = 30
+>>> number_2 = 20
+>>> sum = number_1 + number_2
+>>> sum
+50
+>>> word_1 = "hello"
+>>> word_2 = " world"
+>>> greeting = word_1 + word_2
+>>> greeting
+'hello world'
+>>> greeting.upper()
+'HELLO WORLD'
+>>> greeting.title()
+'Hello World'
